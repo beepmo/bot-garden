5b755a393ee4b480ae60b6c2c2ee547591b39476
beep <beepmo@students.cs.ubc.ca>
2023-01-09 20:39:54 -0800

    process_item function test 2 all pass

--- parse_data.py
+++ parse_data.py
@@ -34,38 +34,39 @@ EXCLUDED_PREFIXES = ['H',  # herbariums
 # store for today, before new csv drops tomorrow
 CACHE = []
 
-# -------------------------------------------------------
-# lists to build into df series
-
-# list of beds
-beds = []
-
-# per-bed data lists in which the ith entry corresponds to the ith bed in beds:
-
-# list of species lists
-species_in_bed = []
-
-# list of numbers
-items_in_bed = []
-l_total_percent = []
-g_total_percent = []
-
-'''# l = labelled
-# g = geo-recorded
-# r = reported after date
-# counts, not percentages'''
-lgr = []
-l = []
-g = []
-lg = []
-not_lg = []
-
 
 # -------------------------------------------------------
 # create one row of attributes for each bed
 
 
 def make_df(genus):  # genus is string
+
+    # -------------------------------------------------------
+    # lists to build into df series
+
+    # list of beds
+    beds = []
+
+    # per-bed data lists in which the ith entry corresponds to the ith bed in beds:
+
+    # list of species lists
+    species_in_bed = []
+
+    # list of numbers
+    items_in_bed = []
+    l_total_percent = []
+    g_total_percent = []
+
+    '''# l = labelled
+    # g = geo-recorded
+    # r = reported after date
+    # counts, not percentages'''
+    lgr = []
+    l = []
+    g = []
+    lg = []
+    not_lg = []
+
     start = tim.time()
 
     # populate the lists bed and species_in_bed
@@ -89,17 +90,6 @@ def make_df(genus):  # genus is string
             # bed is in beds
             bed_location = beds.index(bed)
 
-            # affirmative action for item in known bed
-            def increment_here(list):
-                list[bed_location] = 1  # TODO revert
-
-            try:
-                process_item(increment_here, do_nothing, labelled, georecorded, age)
-            except IndexError:
-                print(f'Length of label totals list: {len(l_total_percent)}\n'
-                      f'Length of geore totals list: {len(g_total_percent)}\n'
-                      f'Length od beds: {len(beds)}')
-
             # update item count
             items_in_bed[bed_location] += 1
 
@@ -108,6 +98,30 @@ def make_df(genus):  # genus is string
             if not species in in_this_bed:
                 in_this_bed.append(species)
 
+            # affirmative action for item in known bed
+            # for some reason this throws invalid syntax when lambda
+            def increment_here(list):
+                list[bed_location] += 1
+                return
+
+            try:
+                process_item(increment_here, do_nothing,
+                             labelled, georecorded, age,
+                             l_total_percent, g_total_percent, lg, lgr, not_lg, l, g)
+            except IndexError:
+                print(f'l: {len(l)}'
+                      f'g: {len(g)}'
+                      f'lg: {len(lg)}'
+                      f'lgr: {len(lgr)}'
+                      f'not_lg: {len(not_lg)}')
+                return
+
+            try:
+                assert l_total_percent[bed_location] <= items_in_bed[bed_location]
+            except AssertionError:
+                print(f'l_total_percent:\n{l_total_percent}\n'
+                      f'items:\n{items_in_bed}')
+                return
         else:
             # bed is not in beds
 
@@ -120,31 +134,19 @@ def make_df(genus):  # genus is string
             # count item as list
             items_in_bed.append(1)
 
-            # add tag baskets
-            if labelled:  # total labelled
-                l_total_percent.append(1)
-                # we have not converted the counts to a percentage yet! will do after knowing item count per bed
+            try:
+                # process_item(append_1, append_0, labelled, georecorded, age)
+                process_item((lambda cur_list: cur_list.append(1)), (lambda cur_list: cur_list.append(0)),
+                             labelled, georecorded, age,
+                             l_total_percent, g_total_percent, lg, lgr, not_lg, l, g)
+            except AttributeError:
 
-                if georecorded:  # labelled & geo-recorded
-                    lg.append(1)
-
-                    if age < FUZZY_AGE:  # labelled & geo-recorded & reported post-fuzzy
-                        lgr.append(1)
-                    else:
-                        lgr.append(0)
-
-                else:
-                    lg.append(0)
-
-            else:
-                l_total_percent.append(0)
-
-            if georecorded:
-                g_total_percent.append(1)
-            else:
-                g_total_percent.append(0)
-
-    assert 'HUBC' not in beds
+                print(f'l: {type(l)}'
+                      f'g: {type(g)}'
+                      f'lg: {type(lg)}'
+                      f'lgr: {type(lgr)}'
+                      f'not_lg: {type(not_lg)}')
+                return
 
     # -------------------------------------------------------
     # turn counts into plottable
@@ -155,7 +157,15 @@ def make_df(genus):  # genus is string
     for j in range(len(beds)):
         # -------------------------------------------------------
         # get tag as int percentage
+        if l_total_percent[j] > items_in_bed[j] * 1.2:
+            print(f'labelled %: \n {l_total_percent[j]}')
+            print(f'geo-rec %: \n {g_total_percent[j]}')
+            print(f'items: \n {items_in_bed[j]}'
+                  f'\n')
+            return
+
         l_total_percent[j] = int(l_total_percent[j] / items_in_bed[j] * 100)
+
         g_total_percent[j] = int(g_total_percent[j] / items_in_bed[j] * 100)
 
         # -------------------------------------------------------
@@ -193,7 +203,7 @@ def make_df(genus):  # genus is string
 
 # negative action for item in new (insofar unknown) bed
 def do_nothing(list):
-    return 0
+    pass
 
 
 # affirmative action for item in new (insofar unknown) bed
@@ -206,18 +216,17 @@ def append_0(list):
     list.append(0)
 
 
-def process_item(yes_action, no_action, labelled, georecorded, age):
-
+def process_item(yes_action, no_action, labelled, georecorded, age, l_total, g_total, lg, lgr, not_lg, l, g):
     if labelled:  # total labelled
-        yes_action(l_total_percent)
+        yes_action(l_total)
         # we have not converted the counts to a percentage yet! will do after knowing item count per bed
     else:
-        no_action(l_total_percent)
+        no_action(l_total)
 
     if georecorded:  # total georecorded
-        yes_action(g_total_percent)
+        yes_action(g_total)
     else:
-        no_action(g_total_percent)
+        no_action(g_total)
 
     if labelled and georecorded:
         yes_action(lg)
@@ -232,15 +241,24 @@ def process_item(yes_action, no_action, labelled, georecorded, age):
     if not (labelled or georecorded):
         yes_action(not_lg)
     else:
-        no_action(lg)
+        no_action(not_lg)
+
+    if labelled and not georecorded:
+        yes_action(l)
+    else:
+        no_action(l)
+
+    if georecorded and not labelled:
+        yes_action(g)
+    else:
+        no_action(g)
 
 
 # -------------------------------------------------------
 # make sure make_df loop is run only once. same with mock
 # I see that it gets run twice anyways: before building flask app and after
-make_df('')
 
-'''
 if len(CACHE) == 0:
-    for g in GENUS:
-        CACHE.append(make_df(g))'''
+    for genus in GENUS:
+        CACHE.append(make_df(genus))
+
